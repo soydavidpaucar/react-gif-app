f100a95ec47eac5bbc670fdc0c1a4d7ff1c2a4fd
David Paucar <soydavidpaucar@gmail.com>
2022-10-29 20:05:16 -0500

    :art: imported component to add new category

--- src/GifApp.jsx
+++ src/GifApp.jsx
@@ -1,7 +1,24 @@
+import { useState } from 'react';
+import { AddCategory } from './components/AddCategory';
+
 export const GifApp = () => {
+  const [categories, setCategories] = useState([]);
+  
+  const handleAddNewCategory = (newCategory) => {
+    setCategories([newCategory, ...categories]);
+  };
+  
   return (
-    <>
-      <h1>GifApp</h1>
-    </>
+    <main className="bg-[#0a192f] p-[60px]">
+      <h1 className="text-[#ccd6f6] text-4xl mb-5">GifApp</h1>
+      
+      <AddCategory onNewCategory={handleAddNewCategory} />
+      
+      <ol>
+        {categories.map((category) => (
+          <li key={category} className="text-[#8892b0]">{category}</li>
+        ))}
+      </ol>
+    </main>
   );
 };
