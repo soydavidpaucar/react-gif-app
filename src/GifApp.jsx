575f3a5867f48431daa86c924f549c80430da0c0
David Paucar <soydavidpaucar@gmail.com>
2022-10-30 11:42:29 -0500

    :lipstick: updated font weight

--- src/GifApp.jsx
+++ src/GifApp.jsx
@@ -14,7 +14,7 @@ export const GifApp = () => {
   
   return (
     <main className="bg-[#0a192f] p-[60px] min-h-screen">
-      <h1 className="text-[#ccd6f6] text-6xl mb-5">GifApp</h1>
+      <h1 className="text-[#ccd6f6] text-6xl mb-5 font-black">GifApp</h1>
       
       <AddCategory onNewCategory={handleAddNewCategory} />
       
