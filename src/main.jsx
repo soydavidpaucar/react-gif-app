d587d64792ff445f4c5fe0d4925812981fadabbd
David Paucar <soydavidpaucar@gmail.com>
2022-10-29 22:29:14 -0500

    :pencil2: fixed import double quotes to single quotes

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import { GifApp } from "./GifApp.jsx";
+import { GifApp } from './GifApp.jsx';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
