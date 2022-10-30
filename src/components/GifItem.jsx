b9cd9d72f8dae3e1ccdfed2d2245825085999d4d
David Paucar <soydavidpaucar@gmail.com>
2022-10-30 11:53:57 -0500

    :lipstick: updated gif footer background color

--- src/components/GifItem.jsx
+++ src/components/GifItem.jsx
@@ -1,6 +1,6 @@
 export const GifItem = ({title, url}) => {
   return (
-    <div className="items-center flex justify-center flex-col bg-white rounded-md mb-[20px] h-[0%] mr-[20px]">
+    <div className="items-center flex justify-center flex-col bg-[#ccd6f6] rounded-md mb-[20px] h-[0%] mr-[20px]">
       <img src={url} alt={title} className="w-full rounded-t-md"/>
       <p className="text-[#0a192f]">{title}</p>
     </div>
