afc7ee966373ea74f4c17d07536c7701797999b2
David Paucar <soydavidpaucar@gmail.com>
2022-10-29 20:38:26 -0500

    :art: created gifgrid component to handle list of gifs and its categories

---
+++ src/components/GifGrid.jsx
@@ -0,0 +1,8 @@
+export const GifGrid = ({category}) => {
+  return (
+    <>
+      <h3 className="text-[#ccd6f6] text-4xl mb-2">{category}</h3>
+      <p className="text-[#8791af] mb-12">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores, eius?</p>
+    </>
+  );
+};
