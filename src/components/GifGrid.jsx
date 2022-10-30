aaf3da57c71b25935d2184d69017c5b758e48ea0
David Paucar <soydavidpaucar@gmail.com>
2022-10-30 11:42:14 -0500

    :art: added loading conditional render and updated category font weight

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -7,7 +7,10 @@ export const GifGrid = ({category}) => {
   
   return (
     <>
-      <h3 className="text-[#ccd6f6] text-4xl mb-2">{category}</h3>
+      <h3 className="text-[#ccd6f6] text-4xl mb-2 font-semibold">{category}</h3>
+      
+      {isLoading && <p className="text-[#ccd6f6] text-1xl">Loading...</p>}
+      
       <div className="text-[#8791af] mb-12 flex flex-row flex-wrap justify-center">
         {images.map((image) => (
           <GifItem key={image.id} {...image} />
