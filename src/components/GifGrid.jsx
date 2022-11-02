5451a357ebe8700aa14ca3a905d6652da94325e4
David Paucar <soydavidpaucar@gmail.com>
2022-11-02 17:32:00 -0500

    :lock: added prop types to the component to required category always

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,3 +1,4 @@
+import PropTypes from 'prop-types';
 import { GifItem } from './';
 import { useFetchGifs } from '../hooks/useFetchGifs.js';
 
@@ -17,4 +18,8 @@ export const GifGrid = ({category}) => {
       </div>
     </>
   );
+};
+
+GifGrid.propTypes = {
+  category: PropTypes.string.isRequired,
 };
